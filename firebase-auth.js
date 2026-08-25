2c73961d1a98a0750da7dd4e2397e7f00718b0a4
Lin13423 <forexlin254@gmail.com>
2026-08-25 08:41:25 +0300

    Delete firebase-auth.js

--- firebase-auth.js
+++
@@ -1,4 +0,0 @@
-// Test stub for the Firebase auth CDN module.
-export const getAuth = (...args) => globalThis.__firebaseMock.getAuth(...args);
-export const onAuthStateChanged = (...args) =>
-  globalThis.__firebaseMock.onAuthStateChanged(...args);
