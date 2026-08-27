3e34e32d63904cad983865def13fe36df2cb2569
Lin13423 <forexlin254@gmail.com>
2026-08-27 14:05:51 +0300

    Delete firebase-database.js

--- firebase-database.js
+++
@@ -1,3 +0,0 @@
-// Test stub for the Firebase database CDN module.
-export const set = (...args) => globalThis.__firebaseMock.set(...args);
-export const ref = (...args) => globalThis.__firebaseMock.ref(...args);
