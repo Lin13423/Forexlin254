9eab78b628a342685222711d7f66cce83974ca1b
Lin13423 <forexlin254@gmail.com>
2026-08-27 14:05:32 +0300

    Delete firebase-app.js

--- firebase-app.js
+++
@@ -1,6 +0,0 @@
-// Test stub for the Firebase app CDN module. Delegates to the mock installed
-// on globalThis.__firebaseMock by the test that is running.
-export const initializeApp = (...args) =>
-  globalThis.__firebaseMock.initializeApp(...args);
-export const getApps = (...args) => globalThis.__firebaseMock.getApps(...args);
-export const getApp = (...args) => globalThis.__firebaseMock.getApp(...args);
