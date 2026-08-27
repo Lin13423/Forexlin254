2e30c6d9df680ee8cdc20ef43367fb1f5dff7489
Lin13423 <forexlin254@gmail.com>
2026-08-27 14:03:38 +0300

    Delete ag-config.js

--- ag-config.js
+++
@@ -1,6 +0,0 @@
-globalThis.AG_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDCkP7LTzdNcvZhp24rRsQirYDCcrsuvOA",
-    authDomain: "assetguard-c8c8c.firebaseapp.com",
-    databaseURL: "https://assetguard-c8c8c-default-rtdb.firebaseio.com",
-    projectId: "assetguard-c8c8c"
-};
